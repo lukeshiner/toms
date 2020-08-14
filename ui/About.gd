f1bf3509e9ea4b73752d1d00e3421b3c62eb658c
Luke Shiner <luke@lukeshiner.com>
2020-08-14 16:18:22 +0100

    feat: Add github link to about page

--- ui/About.gd
+++ ui/About.gd
@@ -4,3 +4,7 @@ extends Node2D
 
 func _on_BackButton_pressed():
 	get_tree().change_scene("res://ui/Menu.tscn")
+
+
+func _on_GithubButton_pressed():
+	OS.shell_open("http://github.com/lukeshiner/toms")
